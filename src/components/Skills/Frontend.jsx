9b5ebeef4e3cda38acc35a886f2b58bf007fc98a
Venu-16 <venumadhav2212@gmail.com>
2025-01-26 20:35:40 +0530

    Initial commit

---
+++ src/components/Skills/Frontend.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+const Frontend = () => {
+  return (
+   <div className="skill__content">
+    <h3 className="skill__title">Frontend developer</h3>
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+
+                </div>
+
+                <div>
+                    <h3 className="skills__name">HTML</h3>
+                    <span className="skills__level">Basic</span>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+
+                </div>
+
+                <div>
+                    <h3 className="skills__name">CSS</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+
+                </div>
+
+                <div>
+                    <h3 className="skills__name">JavaScript</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+
+                </div>
+
+                <div>
+                    <h3 className="skills__name">BootStrap</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+
+                </div>
+
+                <div>
+                    <h3 className="skills__name">Git</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+
+
+                </div>
+
+                <div>
+                    <h3 className="skills__name">React</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+            </div>
+        </div>
+    
+   </div>
+  )
+}
+
+export default Frontend
