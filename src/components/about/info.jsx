4be21bd3e412ba48bed8112535d1933641df92e4
Maile Venu Madhav <146411754+Venu-16@users.noreply.github.com>
2025-09-12 04:27:54 +0000

    about updated

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -5,14 +5,19 @@ const info = () => {
     <div className="about__info grid">
         <div className="about__box">
             <i className="bx bx-award about__icon"></i>
-            <h3 className="about__title">Experience</h3>
-            <span className="about__subtitle">3 Months Working</span>
+            <h3 className="about__title">Project Experience</h3>
+            <span className="about__subtitle">6 Months Working</span>
         </div>
 
         <div className="about__box">
             <i className="bx bx-briefcase-alt"></i>
             <h3 className="about__title">Completed</h3>
-            <span className="about__subtitle">5+ Projects</span>
+            <span className="about__subtitle">5+ Projects in AIML & 2 projects in MERN</span>
+        </div>
+        <div className="about__box">
+            <i className="bx bx-briefcase-alt"></i>
+            <h3 className="about__title">Intrest</h3>
+            <span className="about__subtitle">AI, ML, GEN Ai & DSA</span>
         </div>
     </div>
   )
