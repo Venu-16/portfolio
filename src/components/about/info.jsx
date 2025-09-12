3c9882b0e7e7417bd7de74720011389ef769040d
Maile Venu Madhav <146411754+Venu-16@users.noreply.github.com>
2025-09-12 05:00:11 +0000

    emoji added

--- src/components/about/info.jsx
+++ src/components/about/info.jsx
@@ -15,7 +15,7 @@ const info = () => {
             <span className="about__subtitle">5+ Projects in AIML & 2 projects in MERN</span>
         </div>
         <div className="about__box">
-            <i className="bx bx-briefcase-alt"></i>
+            <i className="bx bx-heart about__icon"></i>
             <h3 className="about__title">Intrest</h3>
             <span className="about__subtitle">AI, ML, GEN Ai & DSA</span>
         </div>
